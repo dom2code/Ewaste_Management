152bc452f74a5190540e3158fe87e214bc445509
dom2code <dom2code@gmail.com>
2022-04-29 12:02:59 +0100

    mail change

--- donation_project/settings.py
+++ donation_project/settings.py
@@ -152,5 +152,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'x21168083.test@gmail.com'
-EMAIL_HOST_PASSWORD = '7305608524'
+EMAIL_HOST_USER = 'cppmailtrigger@gmail.com'
+EMAIL_HOST_PASSWORD = '21168083cpp'
